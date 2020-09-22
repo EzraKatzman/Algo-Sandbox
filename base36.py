aa463da496edab36dd41972277f8a9cee1f30d2d
Ezra Katzman <47403054+EzraKatzman@users.noreply.github.com>
2020-09-21 20:34:55 -0400

    some more notes, progress on skeleton

--- base36.py
+++ base36.py
@@ -2,6 +2,14 @@
 # research solution for this
 # Keep track of values 0-9, A-Z, as 0-36
 
+# input number -> divide by 36 (save number for next iteration), % 36 (convert to 0-Z format, add to beginning of output string) 
+# can get divide and mod with divmod() method
+r = 73
+def to_base_36(r):
+    print("Mod remainder: " + str(divmod(r, 36) [1])) # mod remainder (converted to base36 value)
+    print("Quotient (floor): " + str(divmod(r, 36) [0])) # quotient (pushed back to method)
+to_base_36(r)
 
 # to_int
 # Similar method to converting from haxadecimal/binary to integer
+# Reverse string, break for each element in string - convert to numerical value - *36^[x] + ...
