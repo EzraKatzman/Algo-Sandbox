17ff00244ef8695175e991ce84d6c38706f725c7
Ezra Katzman <47403054+EzraKatzman@users.noreply.github.com>
2020-09-21 21:15:11 -0400

    dict update and encoding near complete

--- base36.py
+++ base36.py
@@ -9,11 +9,56 @@
 # Get the integer quotient for the next iteration.
 # Get the remainder for the hex digit.
 # Repeat the steps until the quotient is equal to 0.
-r = 73
-def to_base_36(r):
-    print("Mod remainder: " + str(divmod(r, 36) [1])) # mod remainder (converted to base36 value)
-    print("Quotient (floor): " + str(divmod(r, 36) [0])) # quotient (pushed back to method)
-to_base_36(r)
+alphabet = {
+    "0" : "0",
+    "1" : "1",
+    "2" : "2",
+    "3" : "3",
+    "4" : "4",
+    "5" : "5",
+    "6" : "6",
+    "7" : "7",
+    "8" : "8",
+    "9" : "9",
+    "10" : "A",
+    "11" : "B",
+    "12" : "C",
+    "13" : "D",
+    "14" : "E",
+    "15" : "F",
+    "16" : "G",
+    "17" : "H",
+    "18" : "I",
+    "19" : "J",
+    "20" : "K",
+    "21" : "L",
+    "22" : "M",
+    "23" : "N",
+    "24" : "O",
+    "25" : "P",
+    "26" : "Q",
+    "27" : "R",
+    "28" : "S",
+    "29" : "T",
+    "30" : "U",
+    "31" : "V",
+    "32" : "W",
+    "33" : "X",
+    "34" : "Y",
+    "35" : "Z"
+}
+
+r = 173
+
+def to_base_36(r, result):
+    result += alphabet[str(divmod(r, 36) [1])]  # mod remainder (converted to base36 value)
+    if divmod(r, 36) [0] == 0:
+        print(result)
+    else:
+        to_base_36(divmod(r, 36) [0], result) # quotient (pushed back to method)
+
+
+to_base_36(r, "")
 
 # to_int
 # Similar method to converting from haxadecimal/binary to integer
